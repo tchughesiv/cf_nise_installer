48f31335bce273a1b15220858e09ea09bb1827d0
Tommy Hughes <tchughesiv@gmail.com>
2016-06-16 11:03:43 -0500

    Update generate_deploy_manifest.sh

--- scripts/generate_deploy_manifest.sh
+++ scripts/generate_deploy_manifest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 NISE_IP_ADDRESS=${NISE_IP_ADDRESS:-`ip addr | grep 'inet .*global' | cut -f 6 -d ' ' | cut -f1 -d '/' | head -n 1`}
-
+cf_version=`echo ${INSTALLER_BRANCH} | sed 's/^v//' | sed 's/^V//'`
 sed "s/192.168.10.10/${NISE_IP_ADDRESS}/g" manifests/template.yml > manifests/deploy.yml
 
 if [ "${NISE_DOMAIN}" != "" ]; then
@@ -10,6 +10,15 @@ if [ "${NISE_DOMAIN}" != "" ]; then
         sed -i '' "s/${NISE_IP_ADDRESS}.xip.io/${NISE_DOMAIN}/g" manifests/deploy.yml
     else
         sed -i "s/${NISE_IP_ADDRESS}.xip.io/${NISE_DOMAIN}/g" manifests/deploy.yml
+    fi
+fi
+
+if [ "${INSTALLER_BRANCH}" != "" ]; then
+    if (! sed --version 1>/dev/null 2>&1); then
+        # not a GNU sed
+        sed -i '' "s/CF_VERSION/${cf_version}/g" manifests/deploy.yml
+    else
+        sed -i "s/CF_VERSION/${cf_version}/g" manifests/deploy.yml
     fi
 fi
 
